ee73a139bf334859526457ad43c5e716c0c1b37c
Corey Butler <corey@coreybutler.com>
2020-01-27 09:21:50 -0600

    Added user API stub

---
+++ src/lib/client.js
@@ -0,0 +1,82 @@
+import NGN from '../../../../../Software/Libraries/NGN/ngn/test/.node/index.js'
+
+if (!NGN.INFO) {
+  NGN.INFO = console.log
+}
+
+class APIClient {
+  #user = null
+  #referrer = null
+  #db = null
+  #uid = null
+
+  constructor () {
+    this.fireAuthEvent = true
+
+    // Initialize Firebase
+    firebase.initializeApp({
+      apiKey: "AIzaSyDq2ExQ1ERY16DfS2HIeLOsUPYOrtkRdKY",
+      authDomain: "metadoc-4ebc3.firebaseapp.com",
+      databaseURL: "https://metadoc-4ebc3.firebaseio.com",
+      projectId: "metadoc-4ebc3",
+      storageBucket: "metadoc-4ebc3.appspot.com",
+      messagingSenderId: "99918371964",
+      appId: "1:99918371964:web:5a08c71bb0e7a50634fdf9",
+      measurementId: "G-W4YWGNRVCV"
+    })
+
+    firebase.auth().onAuthStateChanged(user => {
+      NGN.INFO('AUTH STATE CHANGED')
+
+      if (user) {
+        NGN.INFO('AUTHENTICATED....');
+        this.#user = user
+
+        if (this.fireAuthEvent) {
+          NGN.BUS.emit('user.authenticated', user)
+        }
+
+      } else if (this.authenticated) {
+        this.#user = null
+        NGN.BUS.emit('user.logout')
+      }
+
+      NGN.BUS.emit('authstate.changed')
+    })
+  }
+
+  get user () {
+    if (this.#user === null) {
+      this.#user = firebase.auth().currentUser || null
+    }
+
+    return this.#user
+  }
+
+  get uid () {
+    if (this.#user) {
+      return this.#user.uid
+    }
+
+    return this.#uid
+  }
+
+  get authenticated () {
+    return this.#user !== null
+  }
+
+  register (email, password, callback) {
+    if (typeof callback !== 'function') {
+      callback = e => { throw new e }
+    }
+
+    NGN.BUS.emit('user.verifying')
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => this.#user = firebase.auth().currentUser)
+      .catch(console.error)
+  }
+}
+
+const API = new APIClient()
+
+export { API as default }
